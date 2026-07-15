fcc23b26430c9586bc8a15e68d090a493b92a37e
Neel <neelsatasia1221@tamu.edu>
2026-07-15 14:16:48 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,6 +3,11 @@ import sys
 
 def main():
 
+    valid_commands = set(["echo", "type"])
+
+    builtin_cmnds = set(["echo", "exit", "type"])
+
+
     while True:
         sys.stdout.write("$ ")
 
@@ -13,6 +18,15 @@ def main():
             break
         elif "echo" in command and command.find("echo") == 0:
             print(command[5:])
+        elif "type" in command and command.find("type") == 0:
+            in_commands = command.split(" ")
+
+            if len(in_commands) == 2:
+                if in_commands[1] in builtin_cmnds:
+                    print(in_commands[1] + "is a shell builtin")
+                else:
+                    print(in_commands[1] + ": not found")
+
         else:
             sys.stdout.write(command + ": command not found\n")
     
