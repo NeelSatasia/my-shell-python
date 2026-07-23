a9b56af85e186eb6b7b02f8c83ce157ab887ff92
Neel <neelsatasia1221@tamu.edu>
2026-07-23 12:12:27 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,6 +4,17 @@ from pathlib import Path
 import subprocess as sp
 
 
+builtin_cmnds = set(["echo", "exit", "type", "pwd", "cd"])
+
+TYPE = "type"
+ECHO = "echo"
+EXIT = "exit"
+PWD = "pwd"
+CD = "cd"
+HOME = "~"
+CAT = 'cat'
+LS = 'ls'
+
 def path_exec(filename: str):
     system_path = os.environ.get('PATH')
     path_list = system_path.split(os.pathsep)
@@ -20,162 +31,174 @@ def path_exec(filename: str):
                     return directory
     
     return ""
-    
+
+
+def clean_raw_cmnd(full_cmnd: str):
+    cmnd_params = []
+
+    open_quote = ''
+    backslash = False
+    empty_space = False
+
+    for i in range(len(full_cmnd)):
+        if backslash == False and (full_cmnd[i] == "'" or full_cmnd[i] == '"') and (len(open_quote) == 0 or open_quote == full_cmnd[i]):
+            if len(open_quote) == 0:
+                open_quote = full_cmnd[i]
+                
+                if i == 0 or empty_space:
+                    cmnd_params.append("")
+                    empty_space = False
+
+            else:
+                open_quote = ''
+
+        elif full_cmnd[i] == '\\' and backslash == False and (len(open_quote) == 0 or (open_quote == '"' and i + 1 < len(full_cmnd) and (full_cmnd[i+1] == '"' or full_cmnd[i+1] == '\\'))):
+            backslash = True
+        
+        elif backslash:
+            if empty_space:
+                cmnd_params.append("")
+                empty_space = False
+
+            cmnd_params[-1] += full_cmnd[i]
+            backslash = False
+
+        elif len(open_quote) > 0:
+            cmnd_params[-1] += full_cmnd[i]
+        
+        elif full_cmnd[i] == ' ' and empty_space == False:
+            empty_space = True
+
+        elif full_cmnd[i] != ' ':
+            if i == 0 or empty_space:
+                cmnd_params.append("")
+                empty_space = False
+            
+            cmnd_params[-1] += full_cmnd[i]
+
+    return cmnd_params
+
 
 def main():
-
-    builtin_cmnds = set(["echo", "exit", "type", "pwd", "cd"])
-
-    TYPE = "type"
-    ECHO = "echo"
-    EXIT = "exit"
-    PWD = "pwd"
-    CD = "cd"
-    HOME = "~"
-    CAT = 'cat'
-
 
     while True:
         sys.stdout.write("$ ")
 
-        command = input()
-        command = command.strip()
+        raw_cmnd = input()
+        raw_cmnd = raw_cmnd.strip()
 
-        if command == EXIT:
+        clean_cmnd = clean_raw_cmnd(raw_cmnd)
+
+        if len(clean_cmnd) == 0:
+            continue
+
+        if clean_cmnd[0] == EXIT:
             break
 
-        elif ECHO in command and command.find(ECHO) == 0:
-            result = ""
-            open_quote = ''
-            backslash = False
-
-            for i, val in enumerate(command[5:], start=5):
-                if backslash == False and (val == "'" or val == '"') and (len(open_quote) == 0 or open_quote == val):
-                    if len(open_quote) > 0:
-                        open_quote = ''
+        elif clean_cmnd[0] == ECHO:           
+            if '>' in clean_cmnd or "1>" in clean_cmnd:
+                i = 1
+                while i < len(clean_cmnd):
+                    if clean_cmnd[i] in ['>', '1>']:
+                        break
                     else:
-                        open_quote = val
-
-                elif val == '\\' and (len(open_quote) == 0 or (open_quote == '"' and i + 1 < len(command) and (command[i+1] == '"' or command[i+1] == '\\'))) and backslash == False:
-                    backslash = True
+                        i += 1
                 
-                elif backslash:
-                    result += val
-                    backslash = False
-                
-                elif len(open_quote) > 0 or (len(open_quote) == 0 and ((val == ' ' and len(result) > 0 and result[-1] != ' ') or val != ' ')):
-                    result += val
+                input_txt = " ".join(clean_cmnd[1:i])
 
-            print(result)
+                file_path = Path(clean_cmnd[i+1])
 
-        elif TYPE in command and command.find(TYPE) == 0:
-            in_commands = command.split(" ")
+                with open(file_path, "w") as f:
+                    f.write(input_txt + "\n")
 
-            if len(in_commands) == 2:
-                if in_commands[1] in builtin_cmnds:
-                    print(in_commands[1] + " is a shell builtin")
+            else:
+                print(" ".join(clean_cmnd[1:]))
+
+
+        elif clean_cmnd[0] == TYPE:
+
+            if len(clean_cmnd) == 2:
+                if clean_cmnd[1] in builtin_cmnds:
+                    print(clean_cmnd[1] + " is a shell builtin")
 
                 else:
-                    directory = path_exec(in_commands[1])
+                    directory = path_exec(clean_cmnd[1])
 
                     if directory == '':
-                        print(in_commands[1] + ": not found")
+                        print(clean_cmnd[1] + ": not found")
 
                     else:
-                        print(in_commands[1] + " is " + directory + "/" + in_commands[1])
+                        print(clean_cmnd[1] + " is " + directory + "/" + clean_cmnd[1])
 
-        elif CAT in command and command.find(CAT) == 0:
-            cat_params = []
-            
-            open_quote = ''
-            backslash = False
+        elif clean_cmnd[0] == CAT:
 
-            for i in range(4, len(command)):
-                if backslash == False and (command[i] == "'" or command[i] == '"') and (len(open_quote) == 0 or open_quote == command[i]):
-                    if len(open_quote) == 0:
-                        open_quote = command[i]
-
-                        if command[i-1] == ' ' and command[i-2] != ' ':
-                            cat_params.append("")
-
-                    else:
-                        open_quote = ''
-
-                elif command[i] == '\\' and backslash == False and (len(open_quote) == 0 or (open_quote == '"' and i + 1 < len(command) and (command[i+1] == '"' or command[i+1] == '\\'))):
-                    backslash = True
-                
-                elif backslash:
-                    cat_params[-1] += command[i]
-                    backslash = False
-
-                elif len(open_quote) > 0:
-                    cat_params[-1] += command[i]
-                
-                elif command[i] != ' ':
-                    if command[i-1] == ' ' and command[i-2] != '\\':
-                        cat_params.append("")
-                    
-                    cat_params[-1] += command[i]
-
-            sp.run(["cat"] + cat_params)
-
-        elif command == PWD:
-            print(Path.cwd())
-        
-        elif CD in command and command.find(CD) == 0:
-            in_commands = command.split(" ")
-
-            if in_commands[1] == HOME:
-                os.chdir(Path.home())
-                continue
-
-            try:
-                os.chdir(in_commands[1])
-
-            except FileNotFoundError:
-                print("cd: " + in_commands[1] + ": No such file or directory")
-
-        else:
-            ext_cmnd = ['']
-
-            if command[0] == "'" or command[0] == '"':
-                open_quote = command[0]
-                ext_cmnd[0] = ''
-                backslash = False
+            if '>' in clean_cmnd or '1>' in clean_cmnd:
 
                 i = 1
-                for val in command[1:]:
-                    if val == open_quote:
-                        i += 1
+                while i < len(clean_cmnd):
+                    if clean_cmnd[i] in ['>', '1>']:
                         break
-                    elif open_quote == '"' and val == '\\' and (command[i+1] == '"' or command[i+1] == '\\'):
-                        backslash = True
+                    else:
+                        i += 1
+
+                combined_text = ""
+
+                for j in range(1, i):
                     
-                    elif backslash:
-                        ext_cmnd[0] += val
-                        backslash = False
+                    file_path = Path(clean_cmnd[j])
+
+                    if file_path.is_file():
+                        f = open(file_path)
+                        combined_text += f.read()
 
                     else:
-                        ext_cmnd[0] += val
-                    
-                    i += 1
+                        print(CAT + ": " + clean_cmnd[j] + ": No such file or directory")
 
-                while i < len(command) and command[i] == ' ':
-                    i += 1
-                
-                ext_cmnd += command[i:].split(' ')
+                with open(clean_cmnd[-1], "w") as file:
+                    file.write(combined_text)
 
             else:
-                ext_cmnd = command.split(' ')
+                sp.run(clean_cmnd)
 
-            directory = path_exec(ext_cmnd[0])
+        elif clean_cmnd[0] == PWD:
+            print(Path.cwd())
+        
+        elif clean_cmnd[0] == CD:
+            if clean_cmnd[1] == HOME:
+                os.chdir(Path.home())
+                continue
+            
+            try:
+                os.chdir(clean_cmnd[1])
+
+            except FileNotFoundError:
+                print(CD + ": " + clean_cmnd[1] + ": No such file or directory")
+
+        elif len(clean_cmnd) >= 2 and clean_cmnd[0] == LS and clean_cmnd[1] == "-1":
+
+            if '>' in clean_cmnd or '1>' in clean_cmnd:
+                dir_path = Path(clean_cmnd[2])
+
+                if os.path.isdir(dir_path):
+                    file_path = Path(clean_cmnd[-1])
+                    dir_items = os.listdir(dir_path)
+
+                    dir_items.sort()
+
+                    for i in range(len(dir_items)):
+                        dir_items[i] = dir_items[i] + "\n"
+
+                    with open(file_path, 'w') as file:
+                        file.writelines(dir_items)
+        
+        else:
+            directory = path_exec(clean_cmnd[0])
 
             if directory == '':
-                print(ext_cmnd[0] + ": not found")
+                print(clean_cmnd[0] + ": not found")
 
             else:
-                sp.run(ext_cmnd)
-                continue
+                sp.run(clean_cmnd)
 
 
 if __name__ == "__main__":
